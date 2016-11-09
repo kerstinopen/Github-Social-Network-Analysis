436ab8314914dd858491b74a531f2943d93c4faa
jbon <jeremy.bonvoisin@gmail.com>
2016-11-09 12:25:26 +0100

    extended data extraction and formatting

--- branch-network-mining.py
+++ branch-network-mining.py
@@ -13,6 +13,7 @@
 # install pyGithub with pip install PyGithub
 # install pygithub3 with pip install pygithub3
 # install NetworkX with pip install networkx
+# sub-directory called Results
 #
 # PyGitHub documentation can be found here: 
 # https://github.com/jacquev6/PyGithub
@@ -25,7 +26,7 @@ import pygithub3
 
 import networkx as nx
 import getpass
-
+import random
 import os
 
 # Clear screen
@@ -40,66 +41,70 @@ from xml.etree.ElementTree import Element, SubElement, Comment, ElementTree
 
 def analyse_repo(repository):
     index = 0;
+
+    # creating graphml header
     graphml = Element('graphml', {
             "xmlns":"http://graphml.graphdrawing.org/xmlns",
             "xmlns:xsi":"http://www.w3.org/2001/XMLSchema-instance",
             "xmlns:y":"http://www.yworks.com/xml/graphml",
             "xmlns:yed":"http://www.yworks.com/xml/yed/3",
             "xsi:schemaLocation":"http://graphml.graphdrawing.org/xmlns http://www.yworks.com/xml/schema/graphml/1.1/ygraphml.xsd"})
-    SubElement(graphml, 'key', { "for":"node", "id":"d1", "yfiles.type":"nodegraphics"})
-    SubElement(graphml, 'key', { "for":"edge", "id":"d2", "yfiles.type":"edgegraphics"})
+    SubElement(graphml, 'key', { "for":"node", "id":"nodeStyle", "yfiles.type":"nodegraphics"})
+    SubElement(graphml, 'key', { "for":"edge", "id":"edgeStyle", "yfiles.type":"edgegraphics"})
+    SubElement(graphml, 'key', { "for":"node", "id":"attrAuthor", "attr.name":"author", "attr.type":"string"})
+    SubElement(graphml, 'key', { "for":"node", "id":"attrComment", "attr.name":"comment", "attr.type":"string"})
     graph = SubElement(graphml, 'graph', {"edgedefault":"directed"})
+
+    # initialize color dictionary
+    # each user is associated with a node color
+    colorDictionary = {}
+    
+    # generate network
     for i in repository.get_commits():
-        source = i.sha
+        currentCommitSha = i.sha
+        currentCommitCommitter = i.author.login
         index += 1
-        print ("commit ", index, " : ", source)
-        node = SubElement(graph, "node", {"id" : source})
-        data = SubElement(node, "data", {"key":"d1"})
-        shapeNode = SubElement(data, "y:ShapeNode")
+        print ("commit ", index, " : ", currentCommitSha)
+ 
+        # update color dictionary
+        # if there is no color associated with the user, insert a new random HTML color in the dictionary
+        if currentCommitCommitter not in colorDictionary.keys():
+            r = lambda: random.randint(0,255) 
+            g = lambda: random.randint(0,255)
+            b = lambda: random.randint(0,255)
+            colorDictionary[currentCommitCommitter]='#%02X%02X%02X' % (r(),g(),b())
+     
+        # create a new node
+        node = SubElement(graph, "node", {"id" : currentCommitSha})
+ 
+        # edit node style
+        nodeStyleData = SubElement(node, "data", {"key":"nodeStyle"})
+        shapeNode = SubElement(nodeStyleData, "y:ShapeNode")
         NodeLabel = SubElement(shapeNode, "y:NodeLabel")
-
-        #node shape
         Shape= SubElement(shapeNode, "y:Shape", {"type":"retangle"})
+        Geometry= SubElement(shapeNode, "y:Geometry", {"height":"15.0", "width":"60"})
+        Fill = SubElement(shapeNode, "y:Fill", {"color":colorDictionary[currentCommitCommitter], "transparent":"false"})
+        BorderStyle = SubElement(shapeNode, "y:BoderStyle", {"hasColor":"false", "type":"line", "width":"1.0"}) # does not work and i don't know why :-(
+        NodeLabel.text = str(currentCommitSha[:7])
 
-        # Geometry= SubElement(shapeNode, "y:Geometry", {"height":"30.0"}, {"width":"30.0"}, {"x":"0.0"}, {"y":"0.0"})
+        # add node attributes
+        attributeData = SubElement(node, "data", {"key":"attrAuthor"})
+        attributeData.text = str(i.author.login)
+        attributeData = SubElement(node, "data", {"key":"attrComment"})
+        attributeData.text = str(i.commit.message)
 
-        # Erklärung:
-        # def SubElement(parent, tag, attrib={}, **extra):
-        # **extra Additional attributes, given as keyword arguments.
-
-        Geometry= SubElement(shapeNode, "y:Geometry", {"height":"30.0"}, width="%s" %"30.0", x="%s" %"0.0", y="%s" %"0.0")
-        Fill = SubElement(shapeNode, "y:Fill", {"color":"#FFCC00"}, transparent="%s" %"false")
-        BorderStyle = SubElement(shapeNode, "y:BoderStyle", {"color":"#000000"}, type="%s" %"line", width="%s" %"1.0")
-
-        NodeLabel.text = str(index)
         index2 = 0
         for k in i.parents:
             index2 += 1
-            target = k.sha
+            childCommitSha = k.sha
             edge = SubElement(graphml, "edge", {
                 "id":str(index)+"_"+str(index2),
                 "directed":"true",
-                "source":i.sha,
-                "target":k.sha})
-#           commit = etree.SubElement(root, "commit")
-#
-#            nodes = etree.SubElement(commit, "nodes")
-#            edges = etree.SubElement(commit, "edges")
-#
-#            node = etree.SubElement(nodes, "node", id="%s" %C_sha)
-#            edge = etree.SubElement(edges, "edge", source = "%s" %source, target = "%s" %target)
+                "source":childCommitSha,
+                "target":currentCommitSha})
 
-
-#        newfile = (etree.tostring(root, pretty_print=True, xml_declaration = True, encoding="UTF-8"))
- #       newfile = (etree.tostring(root, pretty_print=True))
-    ElementTree(graphml).write(chosenRepo.name + "_commit_structure.graphml")
-#    print (ElementTree.tostring(graphml))
-
-
-
-
-
-
+    # write the xml file
+    ElementTree(graphml).write("./Results/"+chosenRepo.name + "_commit_structure.graphml")
 
 
 
@@ -110,14 +115,14 @@ if __name__ == "__main__":
     userlogin = input("Login: Enter your username: ")
     password = getpass.getpass("Login: Enter your password: ")
     g = pygithub3.Github( userlogin, password )
-	
+    
     # ask user to repository to analyse
     reponame = input("Enter the name of the repository you want to analyse: ")
-    queryResult = g.search_repositories(reponame)
+    queryResult = g.search_repositories(reponame+ " in:name")
     index = 0
     for i in queryResult:
-	    print ("type ",index," for ",i.name)
-	    index += 1
+        print ("type ",index," for ",i.owner.login,"/",i.name)
+        index += 1
     chosenIndex = input("type a number and press enter: ");
     chosenRepo = queryResult.get_page(0)[int(chosenIndex)]
     print ("Analysis of repository ",chosenRepo.name)
